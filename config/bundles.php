9fc5f2685f082446e81e65e2af8c5cb10e1bee8b
Albert Juhe <albert.juhe@alea.com>
2018-07-19 09:01:27 +0200

    Travel refactor

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    App\Infrastructure\CoreBundle\CoreBundle::class => ['all' => true],
+    App\Infrastructure\TravelBundle\TravelBundle::class => ['all' => true],
 ];
