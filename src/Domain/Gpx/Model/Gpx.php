e02d11a275a7c90fd4359f779cd99c03f300d9d8
Albert Juhe <albert.juhe@alea.com>
2018-08-03 14:18:21 +0200

    Entity decoupling

---
+++ src/Domain/Gpx/Model/Gpx.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: albert.juhe
+ * Date: 31/07/2018
+ * Time: 08:58
+ */
+
+namespace App\Domain\Gpx\Model;
+
+class Gpx
+{
+    private $id;
+
+    private $title;
+
+    private $description;
+
+    private $filename;
+
+    private $color;
+
+    protected $createdAt;
+
+    protected $updatedAt;
+
+    private $travel;
+
+    public function __construct()
+    {
+        $this->updatedAt = new \DateTime;
+        $this->createdAt = new \DateTime;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    /**
+     * @param mixed $filename
+     */
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param mixed $color
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTravel()
+    {
+        return $this->travel;
+    }
+
+    /**
+     * @param mixed $travel
+     */
+    public function setTravel($travel)
+    {
+        $this->travel = $travel;
+    }
+
+
+
+}
