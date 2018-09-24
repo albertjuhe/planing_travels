84f8c2f43fe576cad726172f5a4fad7dfa465693
Albert <albertjuhe@gmail.com>
2018-09-24 19:19:00 +0200

    Domain event structure

--- src/Infrastructure/EventBundle/Repository/DoctrineEventStore.php
+++ src/Infrastructure/EventBundle/Repository/DoctrineEventStore.php
@@ -3,19 +3,55 @@
 
 namespace App\Infrastructure\EventBundle\Repository;
 
+use Symfony\Component\Serializer\Serializer;
 use App\Domain\Common\Model\DomainEvent;
+use App\Domain\Event\Model\StoredEvent;
 use App\Domain\Event\Repository\EventStore;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 class DoctrineEventStore extends ServiceEntityRepository implements EventStore
 {
+    const SERIALIZE_JSON = 'JSON';
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    /**
+     * DoctrineEventStore constructor.
+     * @param ManagerRegistry $registry
+     * @param Serializer $serializer
+     */
+    public function __construct(ManagerRegistry $registry, Serializer $serializer)
+    {
+        parent::__construct($registry, StoredEvent::class);
+        $this->serializer = $serializer;
+    }
+
+
     public function append(DomainEvent $aDomainEvent)
     {
-        // TODO: Implement append() method.
+        $storedEvent = new StoredEvent(
+            get_class($aDomainEvent),
+            $aDomainEvent->occurredOn(),
+            $this->serializer->serialize($aDomainEvent, self::SERIALIZE_JSON)
+        );
+        $this->save($storedEvent);
     }
 
     public function allStoredEventsSince($anEventId)
     {
-        // TODO: Implement allStoredEventsSince() method.
+       $query =$this->createQueryBuilder('e');
+       if ($anEventId) {
+           $query->where('e.id > :eventId')
+               ->setParameter('eventId',$anEventId);
+       }
+       $query->orderBy('e.id');
+    }
+
+    public function save(StoredEvent $storedEvent) {
+        $this->_em->persist($storedEvent);
+        $this->_em->flush($storedEvent);
     }
 
 }
