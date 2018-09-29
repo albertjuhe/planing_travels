e74e542c33b62a8758a3bf0f91d82203a45875bd
Albert <albertjuhe@gmail.com>
2018-09-29 11:11:48 +0200

    UNittesting coverage

---
+++ tests/Model/CurrencyTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Model;
+
+use App\Domain\Money\ValueObject\Currency;
+use PHPUnit\Framework\TestCase;
+
+class CurrencyTest extends TestCase
+{
+    public function testEqualsCurrency()
+    {
+        $currency1 = new Currency('EUR');
+        $currency2 = new Currency('EUR');
+        $this->assertTrue($currency1->equals($currency2));
+        $currency2 = new Currency('USD');
+        $this->assertFalse($currency1->equals($currency2));
+    }
+
+    public function testExpectExceptionInvalidArgumentTest() {
+        $this->expectException(\InvalidArgumentException::class);
+        $currency1 = new Currency('EU');
+
+    }
+
+}
