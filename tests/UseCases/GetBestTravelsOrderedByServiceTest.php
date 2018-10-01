2cfaa3de33bf08e96c3e0b367a073cdcca0307a7
Albert Juhé <albert.juhe@albertjuhe.alea.group>
2018-10-01 09:00:59 +0200

    Travel test

---
+++ tests/UseCases/GetBestTravelsOrderedByServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: albert.juhe
+ * Date: 01/10/2018
+ * Time: 08:46
+ */
+
+namespace App\Tests\UseCases;
+
+
+class GetBestTravelsOrderedByServiceTest
+{
+
+}
