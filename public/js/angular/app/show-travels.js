33ae2acec2c40fe0521ecf430cc4647280c9ac7e
albertjuhe <albertjuhe@gmail.com>
2018-10-26 09:05:27 +0200

    Angular controller

--- public/js/angular/app/show-travels.js
+++ public/js/angular/app/show-travels.js
@@ -1,5 +1,15 @@
-var app = angular.module('ShowTravels', []);
+var app = angular.module('ShowTravelsApp', []);
 
-app.controller('travelController', function ($scope) {
+function TravelController($scope,$http) {
 
-});
+    $scope.publishStateTravel = function (slug) {
+        alert(slug);
+        $http.get("http://travels.localhost/travel/publish/{slug}").then(function(response)
+            {
+                $scope.public = response.data;
+            }
+        )
+    }
+}
+
+app.controller('travelController', TravelController);
