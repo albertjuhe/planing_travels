fc2272c3b86eb948610406dbf2fd734c7c033209
Albert <albertjuhe@gmail.com>
2019-08-22 14:26:22 +0200

    update travel service test

--- src/Application/UseCases/Travel/UpdateTravelService.php
+++ src/Application/UseCases/Travel/UpdateTravelService.php
@@ -49,7 +49,7 @@ class UpdateTravelService implements UsesCasesService
         $user = $commnand->user();
 
         //Only the owner can modify the travel
-        if (!$travel->getUser()->getId() == $user->getId()) {
+        if ($travel->getUser()->getId() !== $user->getId()) {
             throw new InvalidTravelUser();
         }
         DomainEventPublisher::instance()->publish(new TravelWasUpdated($travel->toArray()));
