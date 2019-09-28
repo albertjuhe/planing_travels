a187a356d9999024a4b151d0ba9a0ca34f9aae43
Albert <albertjuhe@gmail.com>
2019-09-28 19:33:23 +0200

    create token jwt service

---
+++ src/Infrastructure/Application/Security/JwtTokenAuthenticator.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Infrastructure\Application\Security;
+
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\AuthorizationHeaderTokenExtractor;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class JwtTokenAuthenticator extends AbstractGuardAuthenticator
+{
+    public function getCredentials(Request $request)
+    {
+        $extractor = new AuthorizationHeaderTokenExtractor(
+            'Bearer',
+            'Authorization'
+        );
+
+        $token = $extractor->extract($request);
+
+        if (!$token) {
+            return;
+        }
+
+        return $token;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        // TODO: Implement getUser() method.
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // TODO: Implement checkCredentials() method.
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        // TODO: Implement onAuthenticationFailure() method.
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // TODO: Implement onAuthenticationSuccess() method.
+    }
+
+    public function supports(Request $request)
+    {
+    }
+
+    public function supportsRememberMe()
+    {
+        // TODO: Implement supportsRememberMe() method.
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // TODO: Implement start() method.
+    }
+}
