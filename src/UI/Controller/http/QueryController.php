f5f8f81ec1c268cfa2e6c687e6f37cc1b5e96770
Albert juhe <albert.juhe@socialpoint.es>
2022-04-09 20:01:48 +0200

    replace UserInterface by User

--- src/UI/Controller/http/QueryController.php
+++ src/UI/Controller/http/QueryController.php
@@ -3,9 +3,11 @@
 namespace App\UI\Controller\http;
 
 use App\Domain\User\Exceptions\UserDoesntExists;
+use App\Domain\User\Model\User;
 use App\Infrastructure\Application\QueryBus\QueryBus;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class QueryController extends AbstractController
 {
@@ -23,7 +25,7 @@ class QueryController extends AbstractController
         return $this->queryBus->query($query);
     }
 
-    public function guard()
+    public function guard(): UserInterface
     {
         $user = $this->security->getUser();
         if (!$user) {
