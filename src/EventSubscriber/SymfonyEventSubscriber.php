332266889a5cbb5f72fb1868eb3ad86016dcc766
Albert Juhe <albert.juhe@socialpoint.es>
2024-09-04 12:33:56 +0200

    add symfony event mangement middleware

---
+++ src/EventSubscriber/SymfonyEventSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Domain\Event\DomainEvent;
+use App\Domain\Event\DomainEventSubscriber;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class SymfonyEventSubscriber implements DomainEventSubscriber
+{
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $dispatcher;
+
+    public function __construct(EventDispatcherInterface $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
+    public function handle(DomainEvent $domainEvent)
+    {
+        $this->dispatcher->dispatch($domainEvent);
+    }
+
+    public function isSubscribedTo(DomainEvent $domainEvent): bool
+    {
+        return true;
+    }
+
+}
--- src/Infrastructure/Middleware/DomainEventsMiddleware.php
+++ src/Infrastructure/Middleware/DomainEventsMiddleware.php
@@ -5,17 +5,26 @@ namespace App\Infrastructure\Middleware;
 use App\Domain\Event\DomainEventPublisher;
 use App\Domain\Event\PersistDomainEventSubscriber;
 use App\Domain\Event\Repository\EventStore;
+use App\EventSubscriber\SymfonyEventSubscriber;
 use League\Tactician\Middleware;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class DomainEventsMiddleware implements Middleware
 {
     protected $eventStore;
 
-    public function __construct(EventStore $eventStore)
-    {
+    public function __construct(
+        EventStore $eventStore,
+        EventDispatcherInterface $dispatcher
+    ) {
         $this->eventStore = $eventStore;
+
         $persistDomainEventSubscriber = new PersistDomainEventSubscriber($this->eventStore);
         DomainEventPublisher::instance()->subscribe($persistDomainEventSubscriber);
+
+        $symfonyEventSubscriber = new SymfonyEventSubscriber($dispatcher);
+        DomainEventPublisher::instance()->subscribe($symfonyEventSubscriber);
     }
 
     public function execute($command, callable $next)
