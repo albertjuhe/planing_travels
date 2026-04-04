6a597d0ba8224b58fca3258eaace5950b3745492
albert.juhe <albert.juhe@socialpoint.es>
2026-04-04 19:58:53 +0200

    calendar with locations

---
+++ src/UI/Controller/API/UpdateLocationVisitDateAPIController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\UI\Controller\API;
+
+use App\Infrastructure\LocationBundle\Repository\DoctrineLocationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+class UpdateLocationVisitDateAPIController extends AbstractController
+{
+    private $locationRepository;
+    private $security;
+    private $em;
+
+    public function __construct(DoctrineLocationRepository $locationRepository, Security $security, EntityManagerInterface $em)
+    {
+        $this->locationRepository = $locationRepository;
+        $this->security = $security;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/api/location/{locationId}/visit-date", name="updateLocationVisitDate", methods={"PATCH"})
+     */
+    public function update(Request $request, string $locationId): JsonResponse
+    {
+        $user = $this->security->getUser();
+        if (empty($user)) {
+            return new JsonResponse(['error' => 'Operation not allowed'], 403);
+        }
+
+        try {
+            $location = $this->locationRepository->findById($locationId);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Location not found'], 404);
+        }
+
+        $travel = $location->getTravel();
+        $travelOwner = $travel->getUser();
+        $isOwner = $travelOwner->getId()->id() === $user->getId()->id();
+        $isShared = false;
+        foreach ($travel->getSharedusers() as $sharedUser) {
+            if ($sharedUser->getId()->id() === $user->getId()->id()) {
+                $isShared = true;
+                break;
+            }
+        }
+
+        if (!$isOwner && !$isShared) {
+            return new JsonResponse(['error' => 'Operation not allowed'], 403);
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $visitAt = isset($data['visitAt']) ? $data['visitAt'] : false;
+
+        if ($visitAt === null || $visitAt === '') {
+            $location->setVisitAt(null);
+        } elseif ($visitAt !== false) {
+            try {
+                $date = new \DateTime($visitAt);
+                $location->setVisitAt($date);
+            } catch (\Exception $e) {
+                return new JsonResponse(['error' => 'Invalid date format'], 400);
+            }
+        }
+
+        $this->em->flush();
+
+        return new JsonResponse(['success' => true]);
+    }
+}
